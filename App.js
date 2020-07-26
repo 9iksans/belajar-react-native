4d96af60411799985c429fa7d4336a85941ba74a
Luqmanul Hakim Iksan <iksan@Luqmanuls-MacBook-Pro.local>
2020-07-26 19:47:28 +0700

    new tab nav

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback}  from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import HomeScreen from './app/screens/HomeScreen';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import HomeScreen from "./app/screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
 function App() {
   console.log("App Executed");
   return (
-    
-  <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false
-      }}> 
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-
-   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
-
 
 export default App;
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,46 +1,116 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, ImageBackground, Image} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import App from '../../App';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ImageBackground,
+  Image,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-function DashboardScreen(){
-    return(
-        <View style={styles.container}>
-            <Text>DashboardScreen</Text>
-        </View>
-    );
+function DashboardScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>DashboardScreen</Text>
+    </View>
+  );
 }
 
-function ProfileScreen(){
-    return(
-        <View style={styles.container}>
-            <Text>ProfileScreen</Text>
-        </View>
-    );
+function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+}
+
+function ActivityScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ActivityScreen</Text>
+    </View>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
-    return(
-        <NavigationContainer independent={true}>
-        <Tab.Navigator>
-          <Tab.Screen name="Dashboard" component={DashboardScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        initialRouteName="Activity"
+        tabBarOptions={{
+          style: {
+            height: 100,
+            paddingTop: 10,
+          },
+          labelStyle: {
+            paddingBottom: 10,
+          },
+          activeTintColor: "#fc5c65",
+        }}
+      >
+        <Tab.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            tabBarLabel: "Dashboard",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Activity"
+          component={ActivityScreen}
+          options={{
+            tabBarLabel: "Activity",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="amazon-alexa"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        alignItems : "center",
-        justifyContent : 'center'
-    },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabBarOptions: {
+    width: 100,
+    height: 200,
+  },
+});
